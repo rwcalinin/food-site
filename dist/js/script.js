0816cc6fbe9b6ea227d6f975e2dfb15e7eff469c
rwcalinin <rwcalinin@gmail.com>
2021-05-19 23:26:08 +0300

    DOTslides

--- dist/js/script.js
+++ dist/js/script.js
@@ -374,6 +374,11 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       totalCount.textContent = slides.length;
     }
+  }
+
+  function changeDotsOpacity() {
+    dotsArr.forEach(dot => dot.style.opacity = '.5');
+    dotsArr[currentSlide - 1].style.opacity = 1;
   } // ? init block ------------------------
 
 
@@ -396,6 +401,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     slidesField.style.transform = `translateX(-${offset}px)`;
     calcPasteCurrentCount();
+    changeDotsOpacity();
   });
   prevButton.addEventListener('click', () => {
     if (offset == 0) {
@@ -406,6 +412,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
     slidesField.style.transform = `translateX(-${offset}px)`;
     calcPasteCurrentCount();
+    changeDotsOpacity();
+  }); // ! slides dotted
+
+  const slider = document.querySelector('.offer__slider'),
+        dots = document.createElement('ol'),
+        dotsArr = [];
+  slider.style.position = 'relative';
+  dots.classList.add('dot-indicators');
+  slider.append(dots);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+         box-sizing: content-box;
+         flex: 0 1 auto;
+         width: 30px;
+         height: 6px;
+         margin-right: 3px;
+         margin-left: 3px;
+         cursor: pointer;
+         background-color: #fff;
+         background-clip: padding-box;
+         border-top: 10px solid transparent;
+         border-bottom: 10px solid transparent;
+         opacity: .5;
+         transition: opacity .6s ease;
+      `;
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+
+    dots.append(dot);
+    dotsArr.push(dot);
+  }
+
+  dotsArr.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      currentSlide = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      changeDotsOpacity();
+      calcPasteCurrentCount();
+    });
   });
 });
 
