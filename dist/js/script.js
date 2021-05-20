9a9c3737881e44e5ad3d00446b2920cb0586bc42
rwcalinin <rwcalinin@gmail.com>
2021-05-20 06:49:56 +0300

    calc lay

--- dist/js/script.js
+++ dist/js/script.js
@@ -458,7 +458,45 @@ document.addEventListener('DOMContentLoaded', () => {
       changeDotsOpacity();
       calcPasteCurrentCount();
     });
-  });
+  }); // ! CalCalc
+
+  const result = document.querySelector('.calculating__result span');
+  let sex, height, weigth, age, ratio;
+
+  function calcTotal() {
+    if (!sex || !height || !weigth || !age || !ratio) {
+      result.textContent = '????';
+      return;
+    }
+
+    if (sex === 'female') {
+      result.textContent = (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+    } else {
+      result.textContent = (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+    }
+  }
+
+  calcTotal();
+
+  function getStaticInfo(parentSelector, activeClass) {
+    const elements = document.querySelectorAll(`${parentSelector} div`);
+    document.querySelector(parentSelector).addEventListener('click', event => {
+      if (event.target.getAttribute('data-ratio')) {
+        ratio = +event.target.getAttribute('data-ratio');
+      } else {
+        sex = event.target.getAttribute('id');
+      }
+
+      console.log(ratio, sex);
+      elements.forEach(elem => {
+        elem.classList.remove('activeClass');
+      });
+      event.target.classList.add('activeClass');
+    });
+  }
+
+  getStaticInfo('#gender', 'calculating__choose-item_active');
+  getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
 });
 
 /***/ })
